dcfc725da156303ceeff0dd8da58654d09903314
Otjosh <jotagetvo@gmail.com>
2023-07-19 04:53:55 -0700

    compute and print the sum

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,16 +2,18 @@
 
 /**
  * main - Entry point
- * Description: Computes and prints the sum of all the multiples
- * of 3 or 5 below 1024 (excluded), followed by a new line.
+ *
+ * Description: Prints the sum of all the multiples of 3 or 5
+ * below 1024 (excluded), followed by a new line.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 int limit = 1024;
-int sum = 0;
+int i, sum = 0;
 
-for (int i = 0; i < limit; i++)
+for (i = 0; i < limit; i++)
 {
 if (i % 3 == 0 || i % 5 == 0)
 {
