115c4e3b5a915f094314663e2fc6d642c51d11a4
Otjosh <jotagetvo@gmail.com>
2023-07-19 06:35:03 -0700

    print_fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,6 +3,9 @@
 
 /**
  * print_fibonacci - Prints the first 50 Fibonacci numbers
+ * The Fibonacci sequence starts with 1 and 2, and each subsequent term is
+ * the sum of the previous two terms. The function prints the numbers in the
+ * sequence, separated by commas and spaces, and followed by a new line.
  */
 void print_fibonacci(void)
 {
@@ -25,6 +28,12 @@ b = c;
 printf("\n");
 }
 
+/**
+ * main - Entry point of the program.
+ * Calls the print_fibonacci function to print the first 50 Fibonacci numbers.
+ *
+ * Return: Always 0 to indicate successful execution of the program.
+ */
 int main(void)
 {
 print_fibonacci();
