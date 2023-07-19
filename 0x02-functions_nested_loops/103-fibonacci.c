4f6b5641d95d6a3cfcef1faf0a014ce7ca2c45f3
Otjosh <jotagetvo@gmail.com>
2023-07-19 06:53:01 -0700

    Sum of even-valued terms in Fibonacci sequence

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,28 +1,35 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: Finds and prints the sum of the even-valued terms
- *              in the Fibonacci sequence whose values do not exceed 4,000,000.
+ * main - Entry point of the program
+ *
+ * Description: Finds the sum of even-valued terms in the Fibonacci sequence
+ *              whose values do not exceed 4,000,000 and prints the sum.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int limit = 4000000;
-int prev = 1, current = 2, next = 0;
-long long sum = 0;
+long int a = 1;
+long int b = 2;
+long int c;
+long int sum = 2;
 
-while (current <= limit)
+while (1)
 {
-if (current % 2 == 0)
-sum += current;
+c = a + b;
+if (c > 4000000)
+break;
 
-next = prev + current;
-prev = current;
-current = next;
+if (c % 2 == 0)
+sum += c;
+
+a = b;
+b = c;
 }
 
-printf("Sum of even-valued terms in Fibonacci sequence: %lld\n", sum);
+printf("Sum of even-valued terms in Fibonacci sequence: %ld\n", sum);
 
 return (0);
 }
