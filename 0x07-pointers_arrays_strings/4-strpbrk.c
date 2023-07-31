60f6c0469e30be0dc48141a2eee45c54259f1703
Otjosh <jotagetvo@gmail.com>
2023-07-31 05:59:13 -0700

    Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,25 +1,27 @@
 #include "main.h"
 
 /**
- * _strpbrk - Entry point
- * @s: input
- * @accept: input
- * Return: Always 0 (Success)
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: The input string to be searched.
+ * @accept: The set of bytes to search for.
+ *
+ * Return: Pointer to the first occurrence in @s of any character from @accept,
+ *         or NULL if no such character is found.
  */
 
 char *_strpbrk(char *s, char *accept)
-
 {
 	int k;
 
 	while (*s)
 	{
 	for (k = 0; accept[k]; k++)
-			{
-			if (*s == accept[k])
-			return (s);
-			}
-		s++;
+	{
+		if (*s == accept[k])
+		return (s);
 	}
+	s++;
+	}
+
 	return ('\0');
 }
