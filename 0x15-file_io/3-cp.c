f169a7ab543675a32cd6c4e7ed1a9f7d4e80ac9f
Otjosh <jotagetvo@gmail.com>
2023-10-30 07:17:18 -0700

    task 3 updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -77,8 +77,8 @@ int main(int argc, char *argv[])
 		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
- 		"Error: Can't read from file %s\n", argv[1]);
-        			free(buffer);
+				"Error: Can't read from file %s\n", argv[1]);
+				free(buffer);
 			exit(98);
 	}
 
